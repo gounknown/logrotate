c9a19203bc679ef9f6dfe7bede6dc6753cc5f94e
wenchy <wenchy.zwz@gmail.com>
2024-04-09 23:04:14 +0800

    typo: fix

--- example_test.go
+++ example_test.go
@@ -19,7 +19,7 @@ func ExampleNew() {
 
 	log.Printf("Hello, World!") // 13 bytes
 	log.Printf("Hello, World!") // 13 bytes
-	time.Sleep(time.Second)     // ensure alerady sink to files
+	time.Sleep(time.Second)     // ensure already sink to files
 	l.Close()
 
 	files, _ := os.ReadDir(dir)
