21e43df1c3c5a9f776849bf1a6fbd99f8abd4743
wenchy <wenchy.zwz@gmail.com>
2024-05-16 00:01:47 +0800

    feat: add dockerfile for testing

---
+++ test/nospace/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gounknown/logrotate"
+)
+
+// test "write: No space left on device"
+func main() {
+	l, err := logrotate.New(
+		"_logs/app.%Y%m%d%H%M.log",
+		logrotate.WithMaxInterval(time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	log.SetOutput(l)
+
+	data := make([]byte, 1024*1024) // 1 MB
+	for {
+		time.Sleep(time.Second)
+		log.Println(data)
+	}
+}
